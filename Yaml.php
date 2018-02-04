6f916ba0b926304b2fedfe4527aa09ba05304036
Guillaume Ponçon <gponcon@gmail.com>
2018-02-04 12:49:13 +0100

    Yaml proxy moved to stream component

---
+++ Yaml.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the OpenStates Framework (osf) package.
+ * (c) Guillaume Ponçon <guillaume.poncon@openstates.com>
+ * For the full copyright and license information, please read the LICENSE file distributed with the project.
+ */
+
+namespace Osf\Stream;
+
+use Osf\Exception\ArchException;
+
+/**
+ * Yaml parser proxy
+ *
+ * @author Guillaume Ponçon <guillaume.poncon@openstates.com>
+ * @copyright OpenStates
+ * @version 1.0
+ * @since OSF-2.0 - 2017
+ * @package osf
+ * @subpackage yaml
+ */
+class Yaml
+{
+    const ENGINE_NATIVE  = 'php';
+    const ENGINE_SYMFONY = 'symfony';
+    const ENGINE_NONE    = null;
+    
+    /**
+     * @param string $yamlStream
+     * @return mixed
+     */
+    public static function parse(string $yamlStream) 
+    {
+        switch (self::getEngine()) {
+            case self::ENGINE_NATIVE : 
+                return yaml_parse($yamlStream);
+            case self::ENGINE_SYMFONY : 
+                return \Symfony\Component\Yaml\Yaml::parse($yamlStream);
+            default : 
+                self::noEngine();
+        }
+    }
+    
+    /**
+     * @param string $yamlFile
+     * @return mixed
+     */
+    public static function parseFile(string $yamlFile)
+    {
+        switch (self::getEngine()) {
+            case self::ENGINE_NATIVE : 
+                return yaml_parse_file($yamlFile);
+            case self::ENGINE_SYMFONY : 
+                return self::parse(file_get_contents($yamlFile));
+            default : 
+                self::noEngine();
+        }
+    }
+    
+    /**
+     * FR: Determine le moteur de traitement des flux yaml
+     * @staticvar string $engine
+     * @return string|null
+     */
+    protected static function getEngine():?string
+    {
+        static $engine = null;
+        
+        if ($engine === null) {
+            if (extension_loaded('yaml')) {
+                $engine = self::ENGINE_NATIVE;
+            } else  {
+                $engine = class_exists('\Symfony\Component\Yaml\Yaml')
+                        ? self::ENGINE_SYMFONY
+                        : self::ENGINE_NONE;
+            }
+        }
+        return $engine;
+    }
+    
+    protected static function noEngine()
+    {
+        throw new ArchException('No engine found for yaml');
+    }
+}
---
+++ Yaml/Test.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the OpenStates Framework (osf) package.
+ * (c) Guillaume Ponçon <guillaume.poncon@openstates.com>
+ * For the full copyright and license information, please read the LICENSE file distributed with the project.
+ */
+
+namespace Osf\Stream\Yaml;
+
+use Osf\Stream\Yaml;
+
+/**
+ * @author Guillaume Ponçon <guillaume.poncon@openstates.com>
+ * @copyright OpenStates
+ * @version 1.0
+ * @since OSF-2.0 - 2017
+ * @package osf
+ * @subpackage test
+ */
+class Test extends \Osf\Test\Runner
+{
+    public static function run()
+    {
+        self::reset();
+        
+        $expected = ['root' => ['a' => 'b', 'c' => 'd', 'e' => ['f', 'g']]];
+        self::assertEqual(Yaml::parseFile(__DIR__ . '/test.yml'), $expected);
+        
+        return self::getResult();
+    }
+}
